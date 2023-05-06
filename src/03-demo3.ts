0e2a6c61f37d554edf0ac50a3a2a7cd0bb4e4093
dibot55 <diebots555@gmail.com>
2023-05-06 00:36:17 -0600

    Tipado

---
+++ src/03-demo3.ts
@@ -0,0 +1,7 @@
+// Problemas de Replicación de variables en TS. Puedes solucionarlas usando IFFES para encerrarlas en el scope
+
+(()=>{
+  let productPrice: number = 12; // Number
+  let productName: string = "Lapiz"; //String
+  let productDescription: string = "Lapiz es un tipo de lápiz usado para escribir en pantalla y en cartón."; //String
+})();
---
+++ src/tipado.ts
@@ -0,0 +1,3 @@
+let productPrice: number = 12; // Number
+let productName: string = "Lapiz"; //String
+let productDescription: string = "Lapiz es un tipo de lápiz usado para escribir en pantalla y en cartón."; //String
