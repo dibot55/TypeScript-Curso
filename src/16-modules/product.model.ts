4b5f120831c30577a6b16dcf7f4b600c2d056e8b
dibot55 <diebots555@gmail.com>
2023-05-07 23:23:30 -0600

    comment added F2

--- src/16-modules/product.model.ts
+++ src/16-modules/product.model.ts
@@ -3,7 +3,7 @@
 export type sizes = "C" | "M" | "L" | "XL";
 
 export type productObject = {
-  name: string,
+  name: string, //click en la propiedad y luego preciona F2!!!!!! + ENTER
   createdAt: Date,
   stock: number,
   size?: sizes
